77daf7e6329d606d56ccff6f7041180461a07968
Makishi Asemota <makish.asemota@gmail.com>
2022-09-20 12:30:12 -0400

    added small details in product and cart page

--- ecommerce/src/components/CartItems.js
+++ ecommerce/src/components/CartItems.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProductPage } from "./ProductPage";
 
 export default function CartItems({ cart, setCart }) {
   const removeFromCart = (productToRemove) => {
     setCart(cart.filter((product) => product !== productToRemove));
   };
+
+  const [message, setMessage] = useState(false);
+
+  if (cart.length === 0) {
+    setMessage(true);
+  } else {
+    setMessage(false);
+  }
+
   return (
     <>
       <h1 className="cart-title">Cart</h1>
+      <h3 style={{ display: message ? "block" : "none" }}>
+        Nothing to see here
+      </h3>
       <div className="cart-container">
         {cart.map((product, idx) => (
           <div key={idx} className="cart-item">
--- ecommerce/src/components/ProductPage.js
+++ ecommerce/src/components/ProductPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { ProductData } from "./ProductImages";
 import CartItems from "./CartItems";
@@ -21,6 +21,20 @@ export default function ProductPage({ setCart, cart }) {
     setCart(newCart);
   };
 
+  const [message, setMessage] = useState(false);
+
+  const addMessage = (product) => {
+    let itemInCart = cart.find((item) => product.name === item.name);
+    if (itemInCart) {
+      alert("Item already in cart. Only one of each item available.");
+    } else {
+      setMessage(true);
+      setTimeout(function () {
+        setMessage(false);
+      }, 3000);
+    }
+  };
+
   return (
     <div>
       <div className="product-container">
@@ -31,9 +45,21 @@ export default function ProductPage({ setCart, cart }) {
         ></img>
         <p className="product-name">{thisProduct.name}</p>
         <p className="product-description">{thisProduct.description}</p>
-        <button type="submit" onClick={() => addToCart(thisProduct)}>
+        <button
+          type="submit"
+          onClick={() => {
+            addToCart(thisProduct);
+            addMessage(thisProduct);
+          }}
+        >
           Add to Cart
         </button>
+        <p
+          className="add-message"
+          style={{ display: message ? "block" : "none" }}
+        >
+          Added to Cart!
+        </p>
       </div>
     </div>
   );
